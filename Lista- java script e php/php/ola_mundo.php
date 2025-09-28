51943a5f6969ad5e144e1ea5a1c7df5802271b71
Tulio Marcos <107963418+tm-dome@users.noreply.github.com>
2025-09-28 13:06:41 -0300

    Add files via upload

---
+++ Lista- java script e php/php/ola_mundo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá Mundo - PHP</title>
+   
+</head>
+<body>
+    <div class="container">
+        <?php
+            // Código PHP para exibir a mensagem
+            $mensagem = "Olá Mundo!";
+            echo "<h1>$mensagem</h1>";
+          
+        ?>
+    </div>
+</body>
+</html>
